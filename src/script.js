10d7efdf00a1a320d3b0fb14628bed2f2cd620c2
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-04-11 22:57:20 +0530

    lil gui

--- src/script.js
+++ src/script.js
@@ -1,5 +1,29 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GSAP from "gsap";
+import GUI from "lil-gui";
+
+/*Debug */
+const gui = new GUI({
+  width: 300,
+  title: "Fuck Debug",
+  closeFolders: true,
+});
+gui.close(); //close the main folder that is Fuck Debug
+
+//will open/close debug only by press the key
+window.addEventListener("keydown", (e) => {
+  if (e.key == "d") {
+    gui.show(gui._hidden);
+  }
+});
+
+const cubeTweaks = gui.addFolder("Awesome Cube");
+// cubeTweaks.close(); //by default it will be closed
+// or can use closeFolders : true in GUI
+//note : we can put folders into folders
+
+const debugObject = {};
 
 /**
  * Base
@@ -10,14 +34,82 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+debugObject.color = "#b91818";
+
 /**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({
+  color: debugObject.color,
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+//range
+cubeTweaks.add(mesh.position, "y", -3, 3, 0.01).name("elevation"); //min , max , how much increase
+cubeTweaks.add(mesh.position, "x", -3, 3, 0.01).name("horizontal"); //min , max , how much increase
+
+//Checkbox
+cubeTweaks.add(mesh, "visible");
+cubeTweaks.add(mesh.material, "wireframe");
+
+//Colors
+//They are tricky one
+//Cons : It does not show the same color as per in debug UI
+// gui.addColor(mesh.material, "color");
+//This is becasue Three JS do some color mangement in order to optimize the rendering
+// How to get rid of this👇
+// onChange method will give us continuous changes in color
+// change the color explicitly to hexdecimal
+// gui.addColor(mesh.material, "color").onChange((value) => {
+//   console.log(value.getHexString()); //We will be getting our hexa color
+//We could paste this color in material.color 0xXXXXX
+//But this way is also not very handy will use a different way
+// });
+
+//Efficient way
+cubeTweaks.addColor(debugObject, "color").onChange((value) => {
+  material.color.set(value);
+});
+
+//Function/Button
+//Same problem can't put right away
+debugObject.spin = () => {
+  GSAP.to(mesh.rotation, {
+    y: mesh.rotation.y + Math.PI * 2,
+    duration: 10,
+  });
+};
+cubeTweaks.add(debugObject, "spin");
+
+//Tweak the subdivision
+// gui.add(geometry, "widthSegments", 1, 20, 1); //We cannot do like this becuase
+//widthSegments are the property that is used to generate the whole geometry only once
+//We can't just change the parameter and expect it to recreate the parameter
+// debugObject.subdivisions = 2;
+// gui.add(debugObject, "subdivisions", 1, 10, 0.1).onChange(() => {
+//   console.log("subdivisions changes");
+// });
+//But wait we width Segments changes in to create a new geometry
+//Building a geometry is a stress for GPU
+//The Change Event can be triggered just if the user drags and drops the range too much
+//Replcae it by finish Change
+debugObject.subdivisions = 2;
+cubeTweaks.add(debugObject, "subdivisions", 1, 10, 0.1).onFinishChange(() => {
+  //to reduce the stress on GPU
+  //dispose the old geometry before creating the new one
+  mesh.geometry.dispose();
+  mesh.geometry = new THREE.BoxGeometry(
+    1,
+    1,
+    1,
+    debugObject.subdivisions,
+    debugObject.subdivisions,
+    debugObject.subdivisions
+  );
+});
 /**
  * Sizes
  */
